c37fc5028d9b5f1b83cd07b41bcfc96140bbd23e
Sam-wiz <samrudhvvandakudri2005@gmail.com>
2024-08-26 01:20:07 +0530

    Modified some files

--- backend/main.js
+++ backend/main.js
@@ -1,8 +1,8 @@
-// main.js
 const express = require('express');
-const warehouseRoutes = require('./routes/warehouse');
-const fleetRoutes = require('./routes/fleet');
-const bookingRoutes = require('./routes/booking');
+const warehouseRoutes = require('./routes_and_controllers/warehouse');
+const fleetRoutes = require('./routes_and_controllers/fleet');
+const bookingRoutes = require('./routes_and_controllers/booking');
+const db = require('./config/mongo_db/dbConfig')
 
 const app = express();
 
@@ -11,6 +11,8 @@ app.use(express.json());
 app.use('/warehouse', warehouseRoutes);
 app.use('/fleet', fleetRoutes);
 app.use('/booking', bookingRoutes);
+
+
 
 app.listen(5000, () => {
   console.log('Server running on port 5000');
