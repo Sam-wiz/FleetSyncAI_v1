62ac57c8d7d892e84621c1d7d05faaa3902ebcc9
Sam-wiz <samrudhvvandakudri2005@gmail.com>
2024-08-26 13:57:59 +0530

    Fixed some errors

--- backend/main.js
+++ backend/main.js
@@ -3,15 +3,18 @@ const warehouseRoutes = require('./routes_and_controllers/warehouse');
 const fleetRoutes = require('./routes_and_controllers/fleet');
 const bookingRoutes = require('./routes_and_controllers/booking');
 const db = require('./config/mongo_db/dbConfig')
+const auth = require('./routes_and_controllers/auth')
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/warehouse', warehouseRoutes);
 app.use('/fleet', fleetRoutes);
 app.use('/booking', bookingRoutes);
-
+app.use('/auth', auth);
 
 
 app.listen(5000, () => {
